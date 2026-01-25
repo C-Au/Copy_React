50318b5e61ccca1b5cba32ab3b05742b8a523801
C-Au <aucolin@hotmail.com>
2026-01-24 20:34:59 -0800

    feat: reorganize App component structure by moving Header above navigation

--- copy_react_app/src/App.jsx
+++ copy_react_app/src/App.jsx
@@ -2,14 +2,16 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import About from "./Components/pages/About/About";
 import Home from "./Components/pages/Home/Home";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div>
-<nav>
-  <Link to="/">Home</Link>
-  <Link to="/about">About</Link>
-</nav>
+      <Header />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
