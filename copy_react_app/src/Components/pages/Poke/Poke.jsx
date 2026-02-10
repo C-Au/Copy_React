e08f53447f9e2e919a8b18b36391c698083c04db
C-Au <aucolin@hotmail.com>
2026-02-09 20:59:53 -0800

    refactor: clean up formatting and improve readability in Poke component

--- copy_react_app/src/Components/pages/Poke/Poke.jsx
+++ copy_react_app/src/Components/pages/Poke/Poke.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PokeCard from "../../PokeCard/PokeCard";
 
@@ -8,90 +8,85 @@ const URL = "https://pokeapi.co/api/v2/pokemon?limit=40";
 // explain the concepts and ideas in this doc in a very simple way that a 12 year old can understand at a basic and then complex level
 
 function Poke() {
-    const [pokemon, setPokemon] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [currLink, setCurrLink] = useState(URL);
-    const [nextLink, setLink] = useState(URL);
-    const [prevLink, setPrevLink] = useState(null);
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [currLink, setCurrLink] = useState(URL);
+  const [nextLink, setLink] = useState(URL);
+  const [prevLink, setPrevLink] = useState(null);
 
-    useEffect(()=>{
-        async function loadPoke() {
-            try {
-                const resp = await axios.get(currLink);
-                
-                setLoading(false);
-                if (resp.status !== "200") {
-                    setError("DID NOT GET 200");
-                }
+  useEffect(() => {
+    async function loadPoke() {
+      try {
+        const resp = await axios.get(currLink);
 
-                console.log(resp)
-
-                console.log(resp.data.next);
-                setLink(resp.data.next);
-                setPrevLink(resp.data.previous);
-
-                // make a list of URLs to be used all together
-                const pokeReqList = resp.data.results.map((p) => {
-                    return axios.get(p.url);
-                });
-                
-                // make a bulk API request to all of the urls
-                const pokeResponse = await Promise.all(pokeReqList);
-
-                // Format the responses from all of the pokemon requests
-                const formatPoke = pokeResponse.map((res) => {
-                    return ({
-                        name: res.data.name,
-                        image: res.data.sprites.front_default
-                    });
-                });
-
-                // Set state to reload the screen
-                setPokemon(formatPoke);
-
-            } catch(err) {
-                console.log(err);
-            }
+        setLoading(false);
+        if (resp.status !== "200") {
+          setError("DID NOT GET 200");
         }
-        loadPoke();
-    }, [currLink]);
 
-    function updateScreenNext() {
-        setCurrLink(nextLink);
+        console.log(resp);
+
+        console.log(resp.data.next);
+        setLink(resp.data.next);
+        setPrevLink(resp.data.previous);
+
+        // make a list of URLs to be used all together
+        const pokeReqList = resp.data.results.map((p) => {
+          return axios.get(p.url);
+        });
+
+        // make a bulk API request to all of the urls
+        const pokeResponse = await Promise.all(pokeReqList);
+
+        // Format the responses from all of the pokemon requests
+        const formatPoke = pokeResponse.map((res) => {
+          return {
+            name: res.data.name,
+            image: res.data.sprites.front_default,
+          };
+        });
+
+        // Set state to reload the screen
+        setPokemon(formatPoke);
+      } catch (err) {
+        console.log(err);
+      }
     }
+    loadPoke();
+  }, [currLink]);
 
-    function updateScreenPrev() {
-        setCurrLink(prevLink);
-    }
+  function updateScreenNext() {
+    setCurrLink(nextLink);
+  }
 
-    if (loading) {
-        return <h2>LOADING ... </h2>
-    }
+  function updateScreenPrev() {
+    setCurrLink(prevLink);
+  }
 
-    if (error) {
-        return <p>{error}</p>
-    }
+  if (loading) {
+    return <h2>LOADING ... </h2>;
+  }
 
-    return <div>
-        <h1>Pokemon LIST!</h1>
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-        { (!prevLink) ? <></> : <button onClick={updateScreenPrev}> Prev </button>}
+  return (
+    <div>
+      <h1>Pokemon LIST!</h1>
 
-        <button onClick={updateScreenNext}> Next </button>
+      {!prevLink ? <></> : <button onClick={updateScreenPrev}> Prev </button>}
 
-        <div style={{display: "flex", flexWrap: "wrap", gap: "2rem"}}>
-            {pokemon.map((p) => {
-                return <PokeCard
-                    name={p.name} 
-                    image={p.image}
-                    key={p.name}
-                />
-            })
-            }
-        </div>
+      <button onClick={updateScreenNext}> Next </button>
+
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "2rem" }}>
+        {pokemon.map((p) => {
+          return <PokeCard name={p.name} image={p.image} key={p.name} />;
+        })}
+      </div>
     </div>
-
+  );
 }
 
 export default Poke;
