e31a0e03dd3c8677cd1af62fa2c35388c4aa326d
Colin Au <44826637+C-Au@users.noreply.github.com>
2026-03-04 14:33:25 -0800

    feat: add PokeType component to display individual Pokemon types

---
+++ copy_react_app/src/Components/PokeType/PokeType.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function PokeType({ typeName, url }) {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [typeObj, setType] = useState(null);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const resp = await axios.get(url);
+        setType(resp.data);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load Pokemon data.");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPokemon();
+  }, []);
+
+  if (loading) return <h2>LOADING ...</h2>;
+  if (error) return <p>{error}</p>;
+  if (!typeObj) return null;
+
+  console.log(typeObj);
+  return (
+    <div style={{ border: "1px solid black", padding: "1rem", margin: "1rem" }}>
+      <p>{typeName}</p>
+      {/* LOOP OVER THE ARRAY WITH A MAP INSTEAD OF HARDCODING THE INDEX VALYES
+      <p>{typeObj.moves[0].name}</p>
+      <p>{typeObj.moves[1].name}</p>
+      <p>{typeObj.moves[2].name}</p>
+      */}
+    </div>
+  );
+}
+
+export default PokeType;
--- copy_react_app/src/Components/Single_Poke/SinglePoke.jsx
+++ copy_react_app/src/Components/Single_Poke/SinglePoke.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import PokeType from "../PokeType/PokeType";
 
 function SinglePoke() {
   const [pokemon, setPokemon] = useState(null);
@@ -25,14 +26,20 @@ function SinglePoke() {
   if (error) return <p>{error}</p>;
   if (!pokemon) return null;
 
-  console.log(pokemon);
+  console.log("TYPE INFO FORM POKEMONM");
+  console.log(pokemon.types[0]);
 
   return (
     <div>
       <h1>Single Pokemon</h1>
       <p>{pokemon.name}</p>
       <img src={pokemon.sprites.back_default} alt="back default" />
+
       <p>Types: {pokemon.types.map((t) => t.type.name).join(", ")}</p>
+
+      {pokemon.types.map((t) => (
+        <PokeType key={t.type.name} typeName={t.type.name} url={t.type.urlx} />
+      ))}
     </div>
   );
 }
